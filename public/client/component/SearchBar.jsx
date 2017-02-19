3eba541c174d125fd3c3ceea797202421da670fd
svey <marcussvehlak.code@gmail.com>
2017-02-18 23:32:58 -0800

    Marcus: Refactored SearchBar, Added jQuery for JSONP request to indeed api, Added state to SearchContainer

--- public/client/component/SearchBar.jsx
+++ public/client/component/SearchBar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var SearchBar = (props) => (
-  <form>
-    <input type='text' placeholder="Search for a Job..." />
-    <input type='submit' value='Search'/>
-  </form>
 
-)
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props)
+    
+  }
+//http://api.indeed.com/ads/apisearch?publisher=5024495540845813&q=software%20engineer&format=json&v=2
+  render() {
+    return (
+    <form onSubmit={this.props.onSubmit}>
+      <input type="text" name="search" placeholder="Job Title..." onChange={this.props.onChange} />
+      <input type="text" pattern="[0-9]{5}" name="location" placeholder="ZIP Code.." onChange={this.props.onChange} />
+      <input type="submit" value="Find Jobs" />
+    </form>
+    )  
+  };
+};
 
-module.exports = SearchBar
+export default SearchBar;
--- public/client/component/SearchContainer.jsx
+++ public/client/component/SearchContainer.jsx
@@ -2,17 +2,48 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import SearchBar from './SearchBar.jsx';
 import Recommend from './Recommend.jsx';
+import axios from 'axios'
 import SearchResultsContainer from './SearchResultsContainer.jsx';
+import $ from 'jQuery'
 
 export default class SearchContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      search: null,
+      location: null
+    }
+    this.getJobs = this.getJobs.bind(this);
+    this.stateHandler = this.stateHandler.bind(this);
   };
+
+  getJobs(event) {
+    console.log(event);
+    event.preventDefault();
+    var context = this;
+
+    //JSONP request to bypass CORS HEADERS
+    $.getJSON("http://api.indeed.com/ads/apisearch?callback=?", {
+      publisher: '5024495540845813',
+      l: this.state.location,
+      q: this.state.search,
+      format: 'json',
+      v: '2'
+    }, function(json){
+      console.log(json.results);
+    });
+  }
+
+  stateHandler(event) {
+    console.log('name: ', event.target.name, 'value: ', event.target.value);
+    this.setState({[event.target.name]: event.target.value})
+  }
+
 
   render() {
     return(
       <div>
-       <SearchBar />
+       <SearchBar onSubmit={this.getJobs} onChange={this.stateHandler} />
        <Recommend recItems={['Software Engineering - Google', 'FrontEnd - Yahoo']}/>
        <SearchResultsContainer/>
       </div>
