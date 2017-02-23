bc2da006f3d4ea9306bcbd05a5fc5768a17715cb
svey <marcussvehlak.code@gmail.com>
2017-02-22 20:00:32 -0800

    Marcus: Added tests for search components.

---
+++ public/client/component/Foo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Foo extends React.Component {
+  render() {
+    return (
+      <div className="foo">
+        <h1 className="bar"></h1>
+        <p className="bar">Bar</p>
+      </div>
+  )}
+}
