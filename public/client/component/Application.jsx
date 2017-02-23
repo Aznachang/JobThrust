e764c44013b00edd0cf7a2010e63cbac30908f07
James Briggs <jfbriggs106@gmail.com>
2017-02-22 16:43:02 -0800

    Add initial functionality for stage changes on click

--- public/client/component/Application.jsx
+++ public/client/component/Application.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 export default class Application extends React.Component {
   constructor(props) {
     super(props);
+    this.nextStage = this.nextStage.bind(this);
+  }
+
+  nextStage() {
+    console.log('Attempting to change stages using ID:', this.props.id);
+    this.props.changeStage(this.props.id);
   }
 
   render() {
     return (
       <tr className="application">
-        <td className="job-title">{this.props.job}</td>
-        <td className="stage">{this.props.stage}</td>
+        <td className="job-title" onClick={this.nextStage}>{this.props.job}</td>
+        <td className="stage" onClick={this.nextStage}>{this.props.stage}</td>
       </tr>
     )
   }
--- public/client/component/manageComponent.jsx
+++ public/client/component/manageComponent.jsx
@@ -9,15 +9,17 @@ export default class ManageComponent extends React.Component {
     this.state = {
       jobs: [],
       stageCounts: {
+        0: 0,
         1: 0,
         2: 0,
         3: 0,
         4: 0,
-        5: 0,
-        6: 0
+        5: 0
       }
     }
     this.getJobs();
+    this.changeStage = this.changeStage.bind(this);
+    this.getStageCounts = this.getStageCounts.bind(this);
   }
 
   getJobs() {
@@ -27,32 +29,64 @@ export default class ManageComponent extends React.Component {
       method: 'GET',
       contentType: 'application/json',
       success: function(data) {
+        data.sort(function(a, b) {
+          return b.stageId - a.stageId;
+        });
         context.setState({
           jobs: data
         });
         console.log('State jobs data:', context.state.jobs);
-        var stageValues = {
-          0: 0,
-          1: 0,
-          2: 0,
-          3: 0,
-          4: 0,
-          5: 0
-        };
-        data.forEach(function(app) {
-          console.log('APP STAGE ID:', app.stageId, typeof app.stageId);
-          stageValues[app.stageId]++;
-        });
-        context.setState({stageCounts: stageValues});
+
+        context.getStageCounts();
+
         console.log('Counts for stages:', context.state.stageCounts);
       }
     })
   }
+
+  getStageCounts() {
+    var jobData = this.state.jobs;
+    var stageValues = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0
+    };
+
+    jobData.forEach(function(app) {
+      stageValues[app.stageId]++;
+    });
+
+    this.setState({stageCounts: stageValues});
+
+  }
+
+  changeStage(id) {
+    console.log('Running changeStage from parent!');
+    var jobsCurrent = this.state.jobs;
+    for (var i = 0; i < jobsCurrent.length; i++) {
+      if (id === jobsCurrent[i].id && jobsCurrent[i].stageId < 5) {
+        console.log('Changing stage ID for:', jobsCurrent[i]);
+        jobsCurrent[i].stageId++;
+        console.log('StageID changed:', jobsCurrent[i]);
+        break;
+      }
+    }
+
+    this.setState({jobs: jobsCurrent});
+    this.getStageCounts();
+
+    this.render();
+  }
+
   render() {
+    var stages = ['Interested', 'Applied', 'Phone Screen','On-Site', 'Decision', 'Offered']
     return (
       <div>
-        <StageList stageCounts={this.state.stageCounts} stages={['Interested', 'Applied', 'Phone Screen','On-Site', 'Decision', 'Offered'] }/>
-        <ApplicationList jobInfo={this.state.jobs} />
+        <StageList stageCounts={this.state.stageCounts} stages={stages}/>
+        <ApplicationList jobInfo={this.state.jobs} stages={stages} changeStage={this.changeStage} />
       </div>
     )
   }
